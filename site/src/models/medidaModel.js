7d0ac2da5f5f827ff00c845f6d02d507030f2800
joaqu1m <joaquim.pires@sptech.school>
2022-06-03 09:47:08 -0300

    api quase pronta

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -14,14 +14,12 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     where fk_aquario = ${idAquario}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `select case
+        when idade = 1 then '1- De 1 a 12'
+        when idade = 2 then '2- De 13 a 20'
+        when idade = 3 then '3- De 21 a 30'
+        when idade = 4 then '4- 31+'
+        end as faixas, count(idade) as 'quantidade' from usuario group by faixas order by 1 desc;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -45,13 +43,7 @@ function buscarMedidasEmTempoReal(idAquario) {
                     order by id desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        instrucaoSql = `select idade from usuario`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -60,13 +52,9 @@ function buscarMedidasEmTempoReal(idAquario) {
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
-function dadosIdade() {
-    instrucaoSql = `select idade from usuario;`
-}
 
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal,
-    dadosIdade
+    buscarMedidasEmTempoReal
 }
--- site/src/routes/medidas.js
+++ site/src/routes/medidas.js
@@ -7,8 +7,8 @@ router.get("/ultimas/:idAquario", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
+// router.get("/tempo-real/:idAquario", function (req, res) {
+//    medidaController.buscarMedidasEmTempoReal(req, res);
+// })
 
 module.exports = router;
